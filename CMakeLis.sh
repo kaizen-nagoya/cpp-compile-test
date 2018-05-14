22e04a19f87a861ea1c5b2ab6dc876e5108f7ccd
kaizen_nagoya <kaizen@wh.commufa.jp>
2018-05-14 21:37:53 +0900

    change include file from first.hpp to first2.hpp.

--- CMakeLis.sh
+++ CMakeLis.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 cp $1/CMakeLists.txt .
+cd basics
+cat myfirstmain.cpp | sed s/myfirst/myfirst2/ > m.cpp
+mv m.cpp myfirstmain.cpp
+cd ..
 mv ./deduce/initlist.cpp ./deduce/initlist2.cpp
 while read line
 do
-cp $1/CMakeLists.txt.$line ./$line/CMakeLists.txt
+  cp $1/CMakeLists.txt.$line ./$line/CMakeLists.txt
 done < $2
